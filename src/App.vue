afc8960e513a56c8007dbe9e1fcc13a9916245c1
S-YamaguchiC <cist.b216.s.yamaguchi@gmail.com>
2019-05-12 16:20:53 +0900

    comment

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/clickerPNG.png">
+    <HelloWorld msg="選択肢があるやろ？選んで押すんやホラあくしろよ"/>
   </div>
 </template>
 
