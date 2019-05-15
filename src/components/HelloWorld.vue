7695483c4004eec7c17e99dd44f63793ebe15390
S-YamaguchiC <cist.b216.s.yamaguchi@gmail.com>
2019-05-15 18:45:47 +0900

    axiosでデータの受け渡しかいてみた(CGIはまだ)

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="hello">
         <h1>{{ msg }}</h1>
-        <form id="n_form" action="CGIのパス" method="post">
+        <form id="n_form">
             <p>選択肢 → [
                 <input type="radio" name="b1" value="A" @click="addData" required>A
                 <input type="radio" name="b1" value="B" @click="addData" required>B
@@ -10,24 +10,40 @@
                 ]
             </p>
         </form>
+        <p v-text="textData">あいうえい</p>
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
     export default {
+
         name: 'HelloWorld',
         props: {
             msg: String
         },
 
+        data() {
+            return {
+                textData: null
+            }
+        },
+
         // つかう関数
         methods: {
-
             // ボタンが押されたときの処理
             addData: function () {
-                alert(document.getElementById("n_form").b1.value)
-                // cgiに値の送信
+                // alert(document.getElementById("n_form").b1.value)
+                var unko = document.getElementById("n_form").b1.value
 
+                // cgiに値の送信
+                axios.get('CGIのパス'+ "?" + unko)
+                    .then(loading => {
+                        this.textData = loading.data.toString()
+                    })
+                    .catch(err => {
+                        this.textData = '送信に失敗したで、ンフフｗ' + '\n : ' + err.toString()
+                    });
             }
         }
     }
